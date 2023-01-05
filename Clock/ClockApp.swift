16d2a63680d51609f8c1ab0d32b9d8385711d381
taksahi <takashi4233@gmail.com>
2023-01-05 10:52:11 +0900

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by TAKASHI YOSHIMURA on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
